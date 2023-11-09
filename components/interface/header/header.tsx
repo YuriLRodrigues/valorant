8b0db7ba76267b949e03f528e06a5d48eb19f21e
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-11-09 16:46:22 -0300

    fix: bug header

--- components/interface/header/header.tsx
+++ components/interface/header/header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
   return (
     <header
       className={`${
-        y && y > 200 ? "fixed z-50 top-0" : "sticky"
+        y && y >= 225 ? "fixed z-50 top-0" : "sticky"
       } w-full h-16 flex justify-between px-4 sm:px-4 py-2 shadow-md dark:shadow-black shadow-zinc-200  dark:bg-zinc-900  z-[40] bg-zinc-100 text-dark dark:text-white`}
     >
       <Logo />
